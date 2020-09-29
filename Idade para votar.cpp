6eecf503ceece70c9834a08f206e5f0231da9caf
Helder Silveira <helder.silveira.dev@gmail.com>
2020-09-29 18:15:25 -0300

    Create Idade para votar.cpp
    Programa para verificar se o usuário tem idade para votar.

---
+++ Idade para votar.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 	int anoAtual, anoNasc, idade;
+ 	cout <<"Ano Atual:";
+ 	cin >>anoAtual;
+ 	cout <<"Ano de Nascimento: ";
+ 	cin>>anoNasc;
+ 	idade=anoAtual-anoNasc;
+ 	cout<<"Você tem ou fará " <<idade<< "anos .";
+ 	if(idade>=16)
+ 	{
+ 		cout<<"Você já pode votar!";
+	 }
+ 	
+ }
+ 
+
+ 
